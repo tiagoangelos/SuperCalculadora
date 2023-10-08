5ff250afe13401d074e876b14b1312d7f8c3099e
Tiago <thiagoangelos2020@gmail.com>
2023-10-08 14:39:42 -0300

    refatorando

--- src/classes/Home.java
+++ src/classes/Home.java
@@ -15,27 +15,28 @@ public class Home extends javax.swing.JFrame {
     private void initComponents() {
 
         jToolBar1 = new javax.swing.JToolBar();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
+        lblTitle = new javax.swing.JLabel();
+        lblInstruction = new javax.swing.JLabel();
         jValue = new javax.swing.JSpinner();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-        jLabel8 = new javax.swing.JLabel();
+        lblIcon = new javax.swing.JLabel();
+        lblTxt1 = new javax.swing.JLabel();
+        lblTxt2 = new javax.swing.JLabel();
+        lblTxt3 = new javax.swing.JLabel();
+        lblTxt4 = new javax.swing.JLabel();
+        lblTxt5 = new javax.swing.JLabel();
         btnCalc = new javax.swing.JButton();
         lblRestDiv = new javax.swing.JLabel();
         lblHighCubic = new javax.swing.JLabel();
         lblRootSquare = new javax.swing.JLabel();
         lblRootCubic = new javax.swing.JLabel();
         lblAbsolutValue = new javax.swing.JLabel();
-        jLabel14 = new javax.swing.JLabel();
+        lblTxt6 = new javax.swing.JLabel();
         lblEvenOrOdd = new javax.swing.JLabel();
         btnExit = new javax.swing.JButton();
-        jMenuBar2 = new javax.swing.JMenuBar();
+        menuBar = new javax.swing.JMenuBar();
         menuAbout = new javax.swing.JMenu();
         menuHelp = new javax.swing.JMenu();
+        menuExit = new javax.swing.JMenu();
 
         jToolBar1.setRollover(true);
 
@@ -43,33 +44,33 @@ public class Home extends javax.swing.JFrame {
         setTitle("Super Calculadora");
         setResizable(false);
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 28)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(51, 51, 51));
-        jLabel1.setText("Super Calculadora");
-        jLabel1.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        lblTitle.setFont(new java.awt.Font("Tahoma", 0, 28)); // NOI18N
+        lblTitle.setForeground(new java.awt.Color(51, 51, 51));
+        lblTitle.setText("Super Calculadora");
+        lblTitle.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
 
-        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel2.setForeground(new java.awt.Color(51, 51, 51));
-        jLabel2.setText("Informe Um Valor:");
+        lblInstruction.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        lblInstruction.setForeground(new java.awt.Color(51, 51, 51));
+        lblInstruction.setText("Informe Um Valor:");
 
         jValue.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
 
-        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Calculator-icon.png"))); // NOI18N
+        lblIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Calculator-icon.png"))); // NOI18N
 
-        jLabel4.setFont(new java.awt.Font("Tahoma", 0, 16)); // NOI18N
-        jLabel4.setText("Resto da Divisão Por 2:");
+        lblTxt1.setFont(new java.awt.Font("Tahoma", 0, 16)); // NOI18N
+        lblTxt1.setText("Resto da Divisão Por 2:");
 
-        jLabel5.setFont(new java.awt.Font("Tahoma", 0, 16)); // NOI18N
-        jLabel5.setText("Elevado ao Cubo:");
+        lblTxt2.setFont(new java.awt.Font("Tahoma", 0, 16)); // NOI18N
+        lblTxt2.setText("Elevado ao Cubo:");
 
-        jLabel6.setFont(new java.awt.Font("Tahoma", 0, 16)); // NOI18N
-        jLabel6.setText("Raiz Quadrada:");
+        lblTxt3.setFont(new java.awt.Font("Tahoma", 0, 16)); // NOI18N
+        lblTxt3.setText("Raiz Quadrada:");
 
-        jLabel7.setFont(new java.awt.Font("Tahoma", 0, 16)); // NOI18N
-        jLabel7.setText("Raiz Cúbica:");
+        lblTxt4.setFont(new java.awt.Font("Tahoma", 0, 16)); // NOI18N
+        lblTxt4.setText("Raiz Cúbica:");
 
-        jLabel8.setFont(new java.awt.Font("Tahoma", 0, 16)); // NOI18N
-        jLabel8.setText("Valor Absoluto:");
+        lblTxt5.setFont(new java.awt.Font("Tahoma", 0, 16)); // NOI18N
+        lblTxt5.setText("Valor Absoluto:");
 
         btnCalc.setFont(new java.awt.Font("Tahoma", 0, 20)); // NOI18N
         btnCalc.setForeground(new java.awt.Color(0, 204, 204));
@@ -100,8 +101,8 @@ public class Home extends javax.swing.JFrame {
         lblAbsolutValue.setForeground(new java.awt.Color(255, 51, 51));
         lblAbsolutValue.setText("0");
 
-        jLabel14.setFont(new java.awt.Font("Tahoma", 0, 16)); // NOI18N
-        jLabel14.setText("Par Ou Impar:");
+        lblTxt6.setFont(new java.awt.Font("Tahoma", 0, 16)); // NOI18N
+        lblTxt6.setText("Par Ou Impar:");
 
         lblEvenOrOdd.setFont(new java.awt.Font("Tahoma", 0, 16)); // NOI18N
         lblEvenOrOdd.setForeground(new java.awt.Color(255, 51, 51));
@@ -122,7 +123,7 @@ public class Home extends javax.swing.JFrame {
                 menuAboutMouseClicked(evt);
             }
         });
-        jMenuBar2.add(menuAbout);
+        menuBar.add(menuAbout);
 
         menuHelp.setText("Ajuda");
         menuHelp.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -130,9 +131,17 @@ public class Home extends javax.swing.JFrame {
                 menuHelpMouseClicked(evt);
             }
         });
-        jMenuBar2.add(menuHelp);
+        menuBar.add(menuHelp);
 
-        setJMenuBar(jMenuBar2);
+        menuExit.setText("Sair");
+        menuExit.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                menuExitMouseClicked(evt);
+            }
+        });
+        menuBar.add(menuExit);
+
+        setJMenuBar(menuBar);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -142,19 +151,19 @@ public class Home extends javax.swing.JFrame {
                 .addGap(2, 2, 2)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 194, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(lblIcon, javax.swing.GroupLayout.PREFERRED_SIZE, 194, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(22, 22, 22)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                                 .addComponent(lblHighCubic, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGroup(layout.createSequentialGroup()
                                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                        .addComponent(jLabel4)
-                                        .addComponent(jLabel5)
-                                        .addComponent(jLabel6)
-                                        .addComponent(jLabel7)
-                                        .addComponent(jLabel8)
-                                        .addComponent(jLabel14))
+                                        .addComponent(lblTxt1)
+                                        .addComponent(lblTxt2)
+                                        .addComponent(lblTxt3)
+                                        .addComponent(lblTxt4)
+                                        .addComponent(lblTxt5)
+                                        .addComponent(lblTxt6))
                                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                         .addGroup(layout.createSequentialGroup()
                                             .addGap(20, 20, 20)
@@ -169,13 +178,13 @@ public class Home extends javax.swing.JFrame {
                             .addGroup(layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                                     .addComponent(btnCalc, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                    .addComponent(lblInstruction, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                                     .addComponent(jValue, javax.swing.GroupLayout.DEFAULT_SIZE, 90, Short.MAX_VALUE)
                                     .addComponent(btnExit, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel1)
+                        .addComponent(lblTitle)
                         .addGap(138, 138, 138)))
                 .addContainerGap(26, Short.MAX_VALUE))
         );
@@ -183,12 +192,12 @@ public class Home extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(21, 21, 21)
-                .addComponent(jLabel1)
+                .addComponent(lblTitle)
                 .addGap(28, 28, 28)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel2)
+                            .addComponent(lblInstruction)
                             .addComponent(jValue, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
@@ -196,29 +205,29 @@ public class Home extends javax.swing.JFrame {
                             .addComponent(btnExit, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(31, 31, 31)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel4)
+                            .addComponent(lblTxt1)
                             .addComponent(lblRestDiv))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel5)
+                            .addComponent(lblTxt2)
                             .addComponent(lblHighCubic))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel6)
+                            .addComponent(lblTxt3)
                             .addComponent(lblRootSquare))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel7)
+                            .addComponent(lblTxt4)
                             .addComponent(lblRootCubic))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel8)
+                            .addComponent(lblTxt5)
                             .addComponent(lblAbsolutValue))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(lblEvenOrOdd)
-                            .addComponent(jLabel14)))
-                    .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 302, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(lblTxt6)))
+                    .addComponent(lblIcon, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 302, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(41, Short.MAX_VALUE))
         );
 
@@ -252,12 +261,28 @@ public class Home extends javax.swing.JFrame {
     }//GEN-LAST:event_btnExitActionPerformed
 
     private void menuAboutMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_menuAboutMouseClicked
-        JOptionPane.showMessageDialog(null, "Super Calculadora é um simples projeto para fornecer\n o maxio de informação matematica sobre um numero informado.");
+        JOptionPane.showMessageDialog(null,
+                "Sobre\n\n"
+              + "Super Calculadora é um simples projeto para\n"
+              + "fornecer o máximo de informação matematica\n"
+              + "sobre um numero informado."
+              , "Sobre", JOptionPane.INFORMATION_MESSAGE
+        );
     }//GEN-LAST:event_menuAboutMouseClicked
 
     private void menuHelpMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_menuHelpMouseClicked
-        JOptionPane.showMessageDialog(null, "Para usar é bem simples informe um numero qualquer\n apos isso basta apenas clicar em 'Calcular' e os\n resultados serão exibidos abaixo.");
+        JOptionPane.showMessageDialog(null,
+                "Ajuda\n\n"
+              + "Para usar é bem simples informe um numero qualquer\n"
+              + "apos isso basta apenas clicar em 'Calcular' e os\n"
+              + "resultados serão exibidos abaixo."
+              , "Ajuda", JOptionPane.INFORMATION_MESSAGE
+        );
     }//GEN-LAST:event_menuHelpMouseClicked
+
+    private void menuExitMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_menuExitMouseClicked
+        System.exit(0);
+    }//GEN-LAST:event_menuExitMouseClicked
 
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -270,25 +295,26 @@ public class Home extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnCalc;
     private javax.swing.JButton btnExit;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel14;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
-    private javax.swing.JMenuBar jMenuBar2;
     private javax.swing.JToolBar jToolBar1;
     private javax.swing.JSpinner jValue;
     private javax.swing.JLabel lblAbsolutValue;
     private javax.swing.JLabel lblEvenOrOdd;
     private javax.swing.JLabel lblHighCubic;
+    private javax.swing.JLabel lblIcon;
+    private javax.swing.JLabel lblInstruction;
     private javax.swing.JLabel lblRestDiv;
     private javax.swing.JLabel lblRootCubic;
     private javax.swing.JLabel lblRootSquare;
+    private javax.swing.JLabel lblTitle;
+    private javax.swing.JLabel lblTxt1;
+    private javax.swing.JLabel lblTxt2;
+    private javax.swing.JLabel lblTxt3;
+    private javax.swing.JLabel lblTxt4;
+    private javax.swing.JLabel lblTxt5;
+    private javax.swing.JLabel lblTxt6;
     private javax.swing.JMenu menuAbout;
+    private javax.swing.JMenuBar menuBar;
+    private javax.swing.JMenu menuExit;
     private javax.swing.JMenu menuHelp;
     // End of variables declaration//GEN-END:variables
 }
